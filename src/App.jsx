2c78cfe90bea4a11a6f3db6e2c425b27687df627
Laurie Roy <laurieroy.dev@gmail.com>
2023-09-01 19:58:43 -0500

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react'
+
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scenes/Navbar";
+// import DotGroup from "./scenes/DotGroup";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [selectedPage, setSelectedPage] = useState("home");
+  const isAboveMediumScreens = 
+    useMediaQuery("(min-width: 1060px)"
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      () => window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      {/* <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+      </div> */}
+    </div>
+  )
 }
 
-export default App;
+export default App
